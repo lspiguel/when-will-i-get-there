c62e79ef526ae157ac727e55572240e15e123e29
lspiguel <46658141+lspiguel@users.noreply.github.com>
2019-10-10 19:43:17 -0300

    Empty project scaffold for ASP.NET

---
+++ Iteration 1/WhenWillIGetThere/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhenWillIGetThere.Startup))]
+namespace WhenWillIGetThere
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
