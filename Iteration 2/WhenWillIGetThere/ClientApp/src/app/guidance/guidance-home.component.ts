bbc659e4dffbc72e058c9c9ca33f67095c47f05f
lspiguel <46658141+lspiguel@users.noreply.github.com>
2019-11-05 22:40:16 -0300

    Iteration 2 - starting with template for SPA Angular + ASP.NET Core 3.0

---
+++ Iteration 2/WhenWillIGetThere/ClientApp/src/app/guidance/guidance-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guidance-home',
+  templateUrl: './guidance-home.component.html',
+  styleUrls: ['./guidance-home.component.css']
+})
+export class GuidanceHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
